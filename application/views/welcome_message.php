50ba4f7437a5303711b3b8774e2ad7beafe66ae0
Tanmay M <masalkar8@gmail.com>
2023-04-30 01:38:40 +0530

    product by cat

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -27,7 +27,7 @@
     foreach($categories as $category){
     ?>
       <div class="image-container">
-      <a href="<?php //echo base_url().'products/'.$category->category_name.'/' ?>"><img class="img-thumbnail" src="<?php echo $category->category_image?>" alt="Image 1">
+      <a href="<?php echo base_url().'products/'.$category->category_name.'/' ?>"><img class="img-thumbnail" src="<?php echo $category->category_image?>" alt="Image 1">
       <p style="word-break: break-word" class="text-center cat_title" ><?php echo ucwords($category->category_name) ?></p>
       </a>
       </div>
