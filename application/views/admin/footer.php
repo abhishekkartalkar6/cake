429da8b223845675aba9fa639c7bce372ab20927
Abhishek Kartalkar <abhishekk001dev@gmail.com>
2023-05-04 10:14:17 +0530

    server data table

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -24,6 +24,10 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
       <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
       <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url();?>/assets/adminjs/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+      
 
 
         <script>
