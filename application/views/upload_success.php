bdd862cd8ffe0e5ca8aff267dbf687e72bba6ea8
abhishekkartalkar6 <125784034+abhishekkartalkar6@users.noreply.github.com>
2023-08-13 17:42:17 +0530

    Create upload_success.php

---
+++ application/views/upload_success.php
@@ -0,0 +1,19 @@
+<?php require_once('header.php') ?>
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">CSV</h1>
+                        <!-- <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Categories</li>
+                        </ol> -->
+                        <!-- upload_form.php -->
+<h6>Successfull Upload</h6>
+
+                        
+                        </div>
+                        <br>
+                        
+                    </div>
+                </main>
+
+                <?php require_once('footer.php') ?>
+                
